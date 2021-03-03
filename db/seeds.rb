861d98c7831439418f013f8f8d7b7c7746392578
floriansaubin <saubinflorian@gmail.com>
2021-03-03 16:50:35 +0100

    Challenge 3

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '29 rue du manchon',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'T2 pas terrible',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'Pas terrible ce T2',
+  price_per_night: 10,
+  number_of_guests: 2
+)
+
+Flat.create!(
+  name: 'La Maison Blanche',
+  address: 'Washington DC',
+  description: 'Y\'a pire frère',
+  price_per_night: 1000,
+  number_of_guests: 25
+)
+
+Flat.create!(
+  name: 'La cabane au fond du jardin',
+  address: 'au fond de mon jardin',
+  description: 'Parfait pour ce ressourcer près de la tondeuse à gazon',
+  price_per_night: 25,
+  number_of_guests: 1
+)
